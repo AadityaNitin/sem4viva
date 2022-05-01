ebd34a28ef7c9bd8370c127cfa2e0d87d4885310
Aaditya <aadityanitin@gmail.com>
2022-05-01 17:14:34 +0530

    Added functions

--- Python/func.py
+++ Python/func.py
@@ -79,3 +79,8 @@
 # using reduce to compute maximum element from list
 # print ("The maximum element of the list is : ",end="")
 # print (functools.reduce(lambda a,b : a if a > b else b,lis))
+
+# #INHERITANCE
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
+# Parent class is the class being inherited from, also called base class.
+# Child class is the class that inherits from another class, also called derived class.
