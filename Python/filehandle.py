1e5bfa01a14696d94dd4153cbcc82f8535592751
Aaditya <aadityanitin@gmail.com>
2022-05-01 20:40:07 +0530

    Added file handling

---
+++ Python/filehandle.py
@@ -0,0 +1,15 @@
+# f = open("C:\\CODE\\sem4viva\\Python\\f1.txt","r")
+# a=f.read()
+# print(a)
+# f.close()
+f1=open("C:\\CODE\\sem4viva\\Python\\f2.txt","w")
+c=f1.write("This text is added using write function")
+f1.close
+f2=open("C:\\CODE\\sem4viva\\Python\\f3.txt","a")
+d="\r" + "This text is appended"
+e=f2.write(d)
+f2.close()
+
+
+
+
