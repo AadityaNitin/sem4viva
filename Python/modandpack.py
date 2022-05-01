9c934b2fb3fa73ac5be114476c26fa3891422942
Aaditya <aadityanitin@gmail.com>
2022-05-01 20:02:54 +0530

    Added modules and packages

---
+++ Python/modandpack.py
@@ -0,0 +1,14 @@
+# The module is a simple Python file that contains collections of functions and global variables and with having a .py extension file. It is an executable file and to organize all the modules we have the concept called Package in Python.
+#We are importing a function myModule from a module(another python file) called demo_module
+#The code in demo_module.py is:
+#def myModule(name):
+	#print("This is My Module : "+ name)
+
+# import demo_module
+# demo_module.myModule("Aaditya")
+
+#PACKAGES:
+#The package is a simple directory having collections of modules. This directory contains Python modules and also having __init__.py file by which the interpreter interprets it as a Package. The package is simply a namespace. The package also contains sub-packages inside it.
+
+#LIBRRARIES:
+#The library is having a collection of related functionality of codes that allows you to perform many tasks without writing your code. It is a reusable chunk of code that we can use by importing it in our program, we can just use it by importing that library and calling the method of that library with period(.)
