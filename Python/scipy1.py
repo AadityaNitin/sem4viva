9d03d432bfc1521067c1c7677b3acbf7cc403495
Aaditya <aadityanitin@gmail.com>
2022-05-01 22:36:52 +0530

    Added Scipy

---
+++ Python/scipy1.py
@@ -0,0 +1,19 @@
+#SciPy is a scientific computation library that uses NumPy underneath.
+# SciPy stands for Scientific Python.
+# It provides more utility functions for optimization, stats and signal processing
+# Like NumPy, SciPy is open source so we can use it freely
+
+import scipy 
+
+#Constants in scipy: We can use some well defined constants such as pi in scipy
+# from scipy import constants
+# print(constants.pi)
+
+#roots of non-linear equation
+# from scipy.optimize import root
+# from math import cos
+# def eqn(x):
+#   return x + cos(x)
+# myroot = root(eqn, 0)
+# print(myroot.x)
+
